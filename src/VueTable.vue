cd9f3052a8eb4cf347636a7a0089d99a6c6d611a
Prasen Shakya <kajiprasen@gmail.com>
2021-01-11 17:07:55 +0545

    initial commit for vue table

---
+++ src/VueTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(column, thIdx) in columns" :key="column.id">
+          <slot :name="`th-${column.id}`" v-bind="{ ...column, thIdx }">
+            {{ column.label }}
+          </slot>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, rowIdx) in items" :key="rowIdx">
+        <td v-for="(column, colIdx) in columns" :key="`${rowIdx}-${colIdx}`">
+          <slot :name="column.id" v-bind="{ ...item, rowIdx, colIdx }">
+            {{ item[column.id] }}
+          </slot>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'VueTable',
+  props: {
+    columns: {
+      type: Array,
+      required: true,
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import VueTable from './VueTable.vue';
 
-createApp(App).mount('#app');
+createApp(VueTable).mount('#app');
